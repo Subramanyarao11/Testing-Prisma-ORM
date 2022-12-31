ff0f346fa67fc945a67a0e66848b6931b6d2ad0f
Subramanyarao11 <subramanya11rao@gmail.com>
2022-12-31 16:45:03 +0530

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/books", async (req, res) => {
+    const books = await prisma.book.findMany();
+    res.json(books);
+});
+
+app.post("/books", async (req, res) => {
+    const { title, author } = req.body;
+    const book = await prisma.book.create({
+        data: {
+            title,
+            author
+        }
+    })
+    res.status(200).json(book);
+})
+// PORT
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Hello from ${PORT} This is a dummy node server`);
+});
